72b12b7637fd1a5d844e98eb17366ea51c00c339
sepiroth-x <45693885+sepiroth-x@users.noreply.github.com>
2023-09-29 18:35:42 +0800

    Profile Browser
    To make this project work, run your own database, and modify the database credentials according to what you have.

---
+++ ProfileBrowser/src/profilebrowser/DatabaseConnection.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package profilebrowser;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Simple Database Query - Profile Browser
+ * Description: Display record from database and move next or previous record
+ * @author sepirothx
+ */
+public class DatabaseConnection {
+
+       private String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+       private String dbConnectionURL = "jdbc:mysql://localhost:3306/javaproject";
+       private String dbUsername = "admin";
+       private String dbPassword = "p@ssword!";
+        
+    
+    public String getJdbcDriver() {
+        return jdbcDriver;
+    }
+
+    public String getDbConnectionURL() {
+        return dbConnectionURL;
+    }
+
+    public String getDbUsername() {
+        return dbUsername;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
+   
+    
+    
+    
+    
+}
---
+++ ProfileBrowser/src/profilebrowser/ProfileBrowser.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package profilebrowser;
+
+/**
+ * Simple Database Query - Profile Browser
+ * Description: Display record from database and move next or previous record
+ * @author sepirothx
+ */
+public class ProfileBrowser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ProfileView pv = new ProfileView();
+        pv.setVisible(true);
+        
+        
+        
+    }
+    
+}
